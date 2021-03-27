3f9952dffa7c97bde13a6495617c6c4fc5f74c8e
chetanchandel31 <chetanchandel3@gmail.com>
2021-03-27 20:32:29 +0530

    implement router and seperate components for navbar & home

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -17,7 +17,7 @@ const Form = ({ currentId, setCurrentId }) => {
 
 	const handleSubmit = e => {
 		e.preventDefault();
-		console.log("sending", postData);
+		// console.log("sending", postData);
 
 		if (currentId) {
 			dispatch(updatePost(currentId, postData));
